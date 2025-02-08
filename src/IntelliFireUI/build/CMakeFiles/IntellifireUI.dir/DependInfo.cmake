b401072b30ffe2956fb74d12aa60b462402204f0
Misael Rivera <misaelrmeneses@outlook.com>
2025-02-08 12:21:04 -0600

    Build with last skeleton UI

---
+++ src/IntelliFireUI/build/CMakeFiles/IntellifireUI.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "IntellifireUI_autogen/timestamp" "custom" "IntellifireUI_autogen/deps"
+  "/home/misaelrivera/IntelliFire/IntelliFireCore/src/IntelliFireUI/build/IntellifireUI_autogen/mocs_compilation.cpp" "CMakeFiles/IntellifireUI.dir/IntellifireUI_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/IntellifireUI.dir/IntellifireUI_autogen/mocs_compilation.cpp.o.d"
+  "/home/misaelrivera/IntelliFire/IntelliFireCore/src/IntelliFireUI/main.cpp" "CMakeFiles/IntellifireUI.dir/main.cpp.o" "gcc" "CMakeFiles/IntellifireUI.dir/main.cpp.o.d"
+  "/home/misaelrivera/IntelliFire/IntelliFireCore/src/IntelliFireUI/mainwindow.cpp" "CMakeFiles/IntellifireUI.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/IntellifireUI.dir/mainwindow.cpp.o.d"
+  "/home/misaelrivera/IntelliFire/IntelliFireCore/src/IntelliFireUI/sensorContainer.cpp" "CMakeFiles/IntellifireUI.dir/sensorContainer.cpp.o" "gcc" "CMakeFiles/IntellifireUI.dir/sensorContainer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/IntelliFireUI/build/CMakeFiles/IntellifireUI_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntellifireUI_autogen"
+  "IntellifireUI_autogen/mocs_compilation.cpp"
+  "IntellifireUI_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/IntellifireUI_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
