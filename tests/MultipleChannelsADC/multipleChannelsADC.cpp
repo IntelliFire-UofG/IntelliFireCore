1e617d1eb2c6948ef4b54b08fc3e800b39efb252
Brenda Camacho <3046006c@student.gla.ac.uk>
2025-02-11 22:16:19 +0000

    Test 4 channel continuous reading

---
+++ tests/MultipleChannelsADC/multipleChannelsADC.cpp
@@ -0,0 +1,94 @@
+/*
+ * ADS1115 test/demo program for the Raspberry PI
+ *
+ * Copyright (c) 2007  MontaVista Software, Inc.
+ * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
+ * Copyright (c) 2013-2024  Bernd Porr <mail@berndporr.me.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
+ */
+
+ #include <stdint.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ #include "ads1015rpi.h"
+ 
+ // We inherit ADS1115rpi, implement
+ // hasSample() and print the ADC reading.
+ class ADS1015Printer : public ADS1015rpi::ADSCallbackInterface {
+private:
+    bool discard = false;
+    ADS1015settings::Input current_channel = ADS1015settings::A0;
+    void printChannel()
+    {
+        switch (current_channel)
+        {
+        case ADS1015settings::A0:
+            printf("A0: ");
+            break;
+        case ADS1015settings::A1:
+            printf("A1: ");
+            break;
+        case ADS1015settings::A2:
+            printf("A2: ");
+            break;
+        case ADS1015settings::A3:
+            printf("A3: ");
+            break;
+        }
+    }
+    void nextChannel()
+    {
+        switch (current_channel)
+        {
+        case ADS1015settings::A0:
+            current_channel = ADS1015settings::A1;
+            break;
+        case ADS1015settings::A1:
+            current_channel = ADS1015settings::A2;
+            break;
+        case ADS1015settings::A2:
+            current_channel = ADS1015settings::A3;
+            break;
+        case ADS1015settings::A3:
+            current_channel = ADS1015settings::A0;
+            break;
+        }
+        setChannel(current_channel);
+    }
+    virtual void hasADS1015Sample(float v) override {
+        if (discard == true)
+        {
+            return;
+            discard = false;
+        }
+        printChannel();
+        printf("%e\n",v);
+        nextChannel();
+        discard = true;
+    }
+ };
+ 
+ // Creates an instance of the ADS1115Printer class.
+ // Prints data till the user presses a key.
+ int main(int argc, char *argv[]) {
+     fprintf(stderr,"Press any key to stop.\n");
+     ADS1115Printer ads1115Callback;
+     ADS1115rpi ads1115rpi;
+     ads1115rpi.registerCallback(&ads1115Callback);
+         ADS1115settings s;
+     s.samplingRate = ADS1115settings::FS64HZ;
+     s.drdy_chip = 4; // for RPI1-4 chip = 0. For RPI5 it's chip = 4.
+     ads1115rpi.start(s);
+         fprintf(stderr,"fs = %d\n",ads1115rpi.getADS1115settings().getSamplingRate());
+     getchar();
+     ads1115rpi.stop();
+     return 0;
+ }
+ 
