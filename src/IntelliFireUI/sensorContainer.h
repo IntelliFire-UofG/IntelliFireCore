ac8af1fe912fe1bbf9dc50a600e3ce333c374c34
Mo <2952593a@student.gla.ac.uk>
2025-02-15 16:23:46 +0000

    updated the header file to match the changes made to the sensorContainer.cpp

--- src/IntelliFireUI/sensorContainer.h
+++ src/IntelliFireUI/sensorContainer.h
@@ -1,6 +1,7 @@
 #ifndef SENSORCONTAINER_H
 #define SENSORCONTAINER_H
-
+#include <atomic>
+#include <QLabel>
 #include <QWidget>
 
 class SensorContainer : public QWidget
@@ -8,6 +9,14 @@ class SensorContainer : public QWidget
     Q_OBJECT
 public:
     explicit SensorContainer(int containerNumber, QWidget *parent = nullptr);
+private slots:
+    int updateSensorValue(); 
+
+
+private:
+    QTimer *timer;  // ✅ Store QTimer in the class to prevent crashes
+    QLabel *sensorLabel; 
+
 };
 
 #endif // SENSORCONTAINER_H
