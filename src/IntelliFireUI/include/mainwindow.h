6f91c2ae6e2e5a2b9aef599b0c7589e39303ecc0
Mo <2952593a@student.gla.ac.uk>
2025-02-25 17:10:41 +0000

    Changed header to include the camera integration and callbacks to libcam

--- src/IntelliFireUI/include/mainwindow.h
+++ src/IntelliFireUI/include/mainwindow.h
@@ -6,18 +6,34 @@
 #include <QKeyEvent>
 #include "sensorContainer.h"
 #include "keyLogger.h"
+#include "libcam2opencv.h"
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    
     explicit MainWindow(QWidget *parent = nullptr);
+    QLabel       *image;
+    void updateImage(const cv::Mat &mat);
     KeyLogger *getKeyLogger();  // Make KeyLogger accessible
     void keyPressEvent(QKeyEvent *event) override;
 
+    struct MyCallback : Libcam2OpenCV::Callback {
+        MainWindow* mainwindow = nullptr;
+        virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
+            if (nullptr != mainwindow) {
+                mainwindow->updateImage(frame);
+            }
+        }
+    };
+    
+    Libcam2OpenCV camera;
+    MyCallback myCallback; 
 
-private slots:
+
+private Q_SLOTS:
     void handleSpeedButton();
     void handleParamButton();
     void updateKeyDisplay(KeyEventInfo keyInfo);
