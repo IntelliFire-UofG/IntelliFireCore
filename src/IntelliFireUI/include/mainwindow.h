e6caebf1744f8bc4055bcfa4103a658aacc5f75d
Mo <2952593a@student.gla.ac.uk>
2025-03-19 13:12:43 +0000

    removed camera from header files

--- src/IntelliFireUI/include/mainwindow.h
+++ src/IntelliFireUI/include/mainwindow.h
@@ -6,7 +6,7 @@
 #include <QKeyEvent>
 #include "sensorContainer.h"
 #include "keyLogger.h"
-#include "libcam2opencv.h"
+
 #include "pumpControl.h"
 
 class MainWindow : public QMainWindow
@@ -16,31 +16,20 @@ class MainWindow : public QMainWindow
 public:
     
     explicit MainWindow(QWidget *parent = nullptr);
-    QLabel       *image;
-    void updateImage(const cv::Mat &mat);
+    
 
     KeyLogger *getKeyLogger();  // Make KeyLogger accessible
     void keyPressEvent(QKeyEvent *event) override;
     void keyReleaseEvent(QKeyEvent *event) override;
 
-    struct MyCallback : Libcam2OpenCV::Callback {
-        MainWindow* mainwindow = nullptr;
-        virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
-            if (nullptr != mainwindow) {
-                mainwindow->updateImage(frame);
-            }
-        }
-    };
     
-    Libcam2OpenCV camera;
-    MyCallback myCallback; 
 
 
 private Q_SLOTS:
     void handleSpeedButton();
     void handleParamButton();
-    void initializeADS1115(SensorContainer *container_1, SensorContainer *container_2, SensorContainer *container_3, SensorContainer *container_4);
-    void updateKeyDisplay(KeyEventInfo keyInfo);
+    // void initializeADS1115(SensorContainer *container_1, SensorContainer *container_2, SensorContainer *container_3, SensorContainer *container_4);
+    // void updateKeyDisplay(KeyEventInfo keyInfo);
     void updatePumpStatus(float pump_status);
 
 private:
