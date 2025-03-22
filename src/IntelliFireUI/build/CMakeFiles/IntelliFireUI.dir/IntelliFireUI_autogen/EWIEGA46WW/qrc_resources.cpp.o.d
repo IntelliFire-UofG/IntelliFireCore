bdf954417a42e861931cdd48eac9d4d261fd6d46
orujovshah <3055419O@student.gla.ac.uk>
2025-03-22 19:21:35 +0000

    Removed CMake Buid files for UI, unified in CMake Build in parent directory.

--- src/IntelliFireUI/build/CMakeFiles/IntelliFireUI.dir/IntelliFireUI_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/IntelliFireUI.dir/IntelliFireUI_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/misaelrivera/IntelliFire/IntelliFireCore/src/IntelliFireUI/build/IntelliFireUI_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
