ee970eb9e7dd31b511fa1244e4321f6acf69d3e6
abood-zm <abdulrahmanzh17@gmail.com>
2025-03-23 17:07:13 +0000

    Update basicMotion to use PWM channels instead of GPIO for motor control

--- src/basicMotionV3.cpp
+++ src/basicMotionV3.cpp
@@ -5,9 +5,23 @@
 void keyboardListener(std::atomic<char>& lastKey);
 void keyboardControl(Motor &leftMotor, Motor &rightMotor, std::atomic<char>& lastKey);
 
+
+// Global motor instances for access by other system components
+std::shared_ptr<Motor> leftMotorInstance = nullptr;
+std::shared_ptr<Motor> rightMotorInstance = nullptr;
+
+// This function can be called from flame detection
+void stopMotors() {
+    if (leftMotorInstance) leftMotorInstance->stop();
+    if (rightMotorInstance) rightMotorInstance->stop();
+}
+
 int basicMotion() {
-    Motor leftMotor(12, 17, 27);                      
-    Motor rightMotor(13, 23, 22);
+    Motor leftMotor(12, 17, 27, 100);                      
+    Motor rightMotor(13, 23, 22, 100);
+
+    leftMotorInstance = std::make_shared<Motor>(leftMotor);
+    rightMotorInstance = std::make_shared<Motor>(rightMotor);
 
     std::atomic<char> lastKey;
     lastKey.store('\0');
